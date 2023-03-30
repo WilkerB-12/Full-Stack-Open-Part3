43a74443e58d6d223ae40156d95a8b0904f4a429
Wilker Blanco <wilkerblanco18@gmail.com>
2023-03-30 02:25:48 -0400

    Exercise 3.14 finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person=require('./models/person');
 
 const app = express();
 app.use(morgan("tiny"));
@@ -8,6 +10,9 @@ app.use(cors());
 app.use(express.static('build'))
 
 app.use(express.json());
+
+
+
 
 let persons = [
   {
@@ -33,7 +38,9 @@ let persons = [
 ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons=>{
+    response.json(persons)
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -44,13 +51,15 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  const id = request.params.id;
+  console.log(id);
+  Person.findById(id).then(person=>{
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -76,17 +85,16 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  idGenerate = Math.floor(Math.random() * 999999999999999999);
-  const person = {
-    id: idGenerate,
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
-  persons = persons.concat(person);
-  response.json(person);
+  });
+ person.save().then(savedPerson=>{
+  response.json(savedPerson)
+ })
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
