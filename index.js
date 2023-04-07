87c0d32a4c635436d0881480285644b2108f39d4
Wilker Blanco <wilkerblanco18@gmail.com>
2023-04-07 12:44:55 -0400

    exercise 3.21 finished

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post("/api/persons", (request, response, next) => {
   });
 });
 
-app.put("/api/persons/:id", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
   const body = request.body;
   const person = {
     name: body.name,
